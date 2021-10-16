896d176062f191f2e36174592787bc0998c3d535
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2021-10-16 20:55:34 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Kushagra Sinha on 16/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
