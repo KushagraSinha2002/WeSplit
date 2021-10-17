2a31858da7d55344645e08181ae40c2c3ac40e67
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2021-10-17 23:33:54 +0530

    -Reading text from the user with TextField

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,17 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
         Form {
             Section {
-                Text("Hello, world!")
+                TextField("Amount", value: $checkAmount, format:
+                    .currency(code: Locale.current.currencyCode ?? "INR"))
+                    .keyboardType(.decimalPad)
             }
             
             Section {
-                Text("Hello, world!")
-                Text("Hello, world!")
+                Text(checkAmount, format:
+                            .currency(code: Locale.current.currencyCode ?? "INR"))
             }
-            
         }
     }
 }
@@ -28,3 +35,12 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+///Section {
+///Text(checkAmount, format:
+///                .currency(code: Locale.current.currencyCode ?? "USD"))
+///}
+
+//.currency(code: "INR"))
